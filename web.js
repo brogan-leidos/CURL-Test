f90c776b45c5c52a77d0e3309de5467569335764
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-24 11:21:13 -0400

    Update web.js

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
+var version = "10.16.1";
+
 var ChampName = "";
 var AP = "";
 var MagicPen = "";
@@ -30,16 +32,47 @@ function getInputs() {
 
 // fetch url: http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/<champname>.json
 function fetchChampion(name) {
-  var fetchUrl = ` http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion/${name}.json`
+  var fetchUrl = ` http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion/${name}.json`
   var data = "";
   fetch(fetchUrl)
     .then(response => response.json())
     .then(fetchData => data = fetchData);
   
-  displayInArea(data);
+  var abilities = parseChampionAbilities(JSON.parse(data).data[name]);
+  displayInArea(abilities);
+}
+
+parseChampionAbilities(champion) {  
+  var passiveTemplate = `http://ddragon.leagueoflegends.com/cdn/${version}/img/passive/{0}`;
+  var spellTemplate = `http://ddragon.leagueoflegends.com/cdn/${version}/img/spell/{0}`;
+
+  var passiveText = champion.passive.description;
+  var passiveImage = passiveTemplate.replace("{0}", champion.passive.image.full);
+  
+  var qText = champion.spells[0].tooltip;
+  var qImage = spellTemplate.replace("{0}", champion.spells[0].image.full);
+  
+  var wText = champion.spells[1].tooltip;
+  var wImage = spellTemplate.replace("{0}", champion.spells[1].image.full);
+
+  var eText = champion.spells[2].tooltip;
+  var eImage = spellTemplate.replace("{0}", champion.spells[2].image.full);
+
+  var rText = champion.spells[3].tooltip;
+  var rImage = spellTemplate.replace("{0}", champion.spells[3].image.full);
+
+    
+  var retString = `<image src=${passiveImage} /> ${passiveText} <br>
+                    <image src=${qImage} /> ${qText} <br>
+                    <image src=${wImage} /> ${wText} <br>
+                    <image src=${eImage} /> ${eText} <br>
+                    <image src=${rImage} /> ${rText} <br>`;
 }
 
 function displayInArea(string) {
   var area = document.getElementById("displayArea");
   area.innerHTML = string;
 }
+
+//http://ddragon.leagueoflegends.com/cdn/10.16.1/img/passive/Anivia_P.png
+//http://ddragon.leagueoflegends.com/cdn/10.16.1/img/spell/FlashFrost.png
